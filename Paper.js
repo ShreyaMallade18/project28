dce27c15bb94d63ee43a3d745a149931f937e0a8
ShreyaMallade18 <79143641+ShreyaMallade18@users.noreply.github.com>
2021-05-31 11:56:16 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,31 @@
+class Paper{
+    constructor(x,y,width,height){
+        var options = {
+             isStatic: true,
+            'restitution':0.3,
+            'friction':0.5,
+            'density': 1.2,  
+        }
+        this.image = loadImage("paper.png");
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        
+        World.add(world, this.body);
+
+       
+
+    }
+
+    display(){
+       imageMode(CENTER);
+
+        fill("yellow");
+
+        image(this.image,this.body.position.x, this.body.position.y, this.width,this.height);
+
+
+
+
+    }
+};
---
+++ launcher.js
@@ -0,0 +1,37 @@
+class launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length:25
+        }
+        this.pointB = pointB
+        this.paper = Constraint.create(options);
+        World.add(world, this.paper);
+    }
+
+    fly(){
+        this.paper.bodyA = null;
+    }
+
+    display(){
+        if(this.paper.bodyA){
+            var pointA = this.paper.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            if(pointA.x < 220){
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y);
+               
+            }
+            else{
+                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x+25, pointA.y, pointB.x+30, pointB.y);
+               
+            }
+            
+        }
+    }
+    
+}
